be82baa7005b1fc6951e553f9584966b7777b9a8
Almuheetu Shihab <almuheetu@gmail.com>
2023-08-27 01:22:09 +0600

    js fix

---
+++ main.js
@@ -0,0 +1,6 @@
+const accessKey = "AW07Cfzc98KJzEBcbjCvb-nkE778yZIhT1cAAh1eGak"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
